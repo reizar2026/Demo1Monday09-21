c4ef15d29e877669f5156e4933cea136345de715
Reidar Nilsen <reidar.nilsen@zocom.>
2026-09-21 09:41:43 +0200

    Add project files.

---
+++ Demo1Monday09-21/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Demo1Monday09_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du");
+            string namn = Console.ReadLine() ?? "";
+
+            bool resultat = LieDetector();
+            if (resultat == true)
+            {
+                Console.WriteLine("Du talar sanning");
+            }
+            else
+            {
+                Console.WriteLine("Du ljuger");
+            }
+        }
+        static bool LieDetector()
+        {
+            Random random = new Random();
+            int resultat = random.Next(0, 2);
+
+            return resultat == 1;
+        }
+    }
+}
